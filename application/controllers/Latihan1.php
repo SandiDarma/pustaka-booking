05e83cf3609ae461d2b89416c2c8506c908e3a7a
SANDI DARMA <78521069+sanskuys@users.noreply.github.com>
2022-03-22 20:46:57 +0700

    Update Latihan1.php

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -4,7 +4,7 @@ class Latihan1 extends CI_Controller
     public function index()
     {
         echo "Selamat datang.. Selamat Belajar Web Programming";
-        //$this->load->view('view-latihan1');
+        //$this->load->view('view-latihan');
     }
 
     public function penjumlahan($n1, $n2)
@@ -13,7 +13,7 @@ class Latihan1 extends CI_Controller
         $data['nilai1'] = $n1;
         $data['nilai2'] = $n2;
         $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);
-        $this->load->view('view-latihan1', $data);
+        $this->load->view('view-latihan', $data);
 
     }
 }
